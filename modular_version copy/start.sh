c84960edf8e4f27b90f1b37b4784a668cb1332e3
weixueyuan <yuanxuewei@proton.me>
2025-11-07 17:40:32 +0800

    结构调整

--- modular_version copy/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /root/apps/miniforge3/etc/profile.d/conda.sh
-conda activate tool
-cd "/root/projects/object_attributes_annotation_tool/modular_version copy/src"
-python main.py --data_file ../merged_attributes.jsonl --uid user1 --port 7800
